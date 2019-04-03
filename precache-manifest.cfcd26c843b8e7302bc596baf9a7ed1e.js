d576e65da701453712edd98f5dcb8ea7aed4f774
David Potsiadlo <davidpots@gmail.com>
2019-04-03 15:01:37 -0500

    Updates

---
+++ precache-manifest.cfcd26c843b8e7302bc596baf9a7ed1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b98e322c192b4e08d1a9",
+    "url": "/react-test/static/css/main.3583c0be.chunk.css"
+  },
+  {
+    "revision": "b98e322c192b4e08d1a9",
+    "url": "/react-test/static/js/main.2580ea41.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-test/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df530e2f25fcc6d8a67e5b6fcf97f60c",
+    "url": "/react-test/index.html"
+  }
+];
